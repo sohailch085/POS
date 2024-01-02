beb8a30219ab18a47b5c2d7f888dce5e211bc947
Sohail Ahmad <74949631+sohailch085@users.noreply.github.com>
2024-01-02 18:45:02 +0500

    POS-02 remove space

--- POS/wwwroot/CutomJquery/Traceability.js
+++ POS/wwwroot/CutomJquery/Traceability.js
@@ -691,7 +691,7 @@ function createCard(data, counter, avilabecontrol) {
                       <div class="row" style="float: right;margin-top: -246px;margin-right: -54px;">
 						<label class="col-sm-3 col-form-label"></label>
 						<div class="col-sm-9" id="button-${counter}">							
-                             <svg id="plusIcon-${counter}" name=${data.ID} xmlns = "http://www.w3.org/2000/ svg" width = "16" height = "16" fill =                                "currentColor" class="bi bi-plus-circle" viewBox = "0 0 16 16" style="margin-top:91px;"> 
+                             <svg id="plusIcon-${counter}" name=${data.ID} xmlns = "http://www.w3.org/2000/ svg" width = "16" height = "16" fill ="currentColor" class="bi bi-plus-circle" viewBox = "0 0 16 16" style="margin-top:91px;"> 
                                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
                                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1                           0-1h3v-3A.5.5 0 0 1 8 4" />
                              </svg>
